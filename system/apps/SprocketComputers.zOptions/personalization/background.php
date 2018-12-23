6666df1cbf7c4ab722bc1f13a95eb284dec7216f
Bryce <kitkatzecat@outlook.com>
2018-12-22 18:44:59 -0600

    Fixed not loading current background correctly

--- system/apps/SprocketComputers.zOptions/personalization/background.php
+++ system/apps/SprocketComputers.zOptions/personalization/background.php
@@ -15,7 +15,7 @@ var OptionsTree = [
 <body class="FlyUiText FlyUiNoSelect">
 
 <div class="title">Change your background</div>
-<div class="box"><div style="margin:0 auto;width:320px;height:180px;background-image:url('<?php echo $_FLY['URL'].$_FLY_USER->visual->theme->backgroundImage; ?>');background-size:cover;background-position:center center;"></div></div>
+<div class="box"><div style="margin:0 auto;width:320px;height:180px;background-image:url('<?php echo FlyVarsReplace((string)$_FLY_USER->visual->theme->backgroundImage,true); ?>');background-size:cover;background-position:center center;"></div></div>
 <p><a><img class="inline-icon" src="<?php echo $_FLY['RESOURCE']['URL']['ICONS']; ?>browse.svg">Choose a new image</a></p>
 <div class="buttons"><button><img style="width:16px;height:16px;vertical-align:middle;margin-right:6px;" src="<?php echo $_FLY['RESOURCE']['URL']['ICONS']; ?>save.svg">Save</button></div>
 
