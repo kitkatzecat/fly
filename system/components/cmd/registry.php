841ac0ec16e237fde7626b1c62c00a12d18fd4f3
Bryce <kitkatzecat@outlook.com>
2018-12-26 02:22:03 -0600

    Full implementation of registry manipulation via commands

--- system/components/cmd/registry.php
+++ system/components/cmd/registry.php
@@ -3,6 +3,130 @@
 include 'Fly.Registry.php';
 include 'Fly.FileProcessor.php';
 
-FlyCommandDisplay('Coming soon...');
+switch($cmd[0]) {
+	case 'get':
+		// 0: "get", 1: key, 2: applicaition
+		$value = FlyRegistryGet($cmd[1],$cmd[2]);
+		if (!$value) {
+			$value = 'false';
+		}
+		FlyCommandDisplay($value);
+		FlyCommandReturn($value);
+		break;
+	
+	case 'gget':
+		$value = FlyGlobalRegistryGet($cmd[1],$cmd[2]);
+		if (!$value) {
+			$value = 'false';
+		}
+		FlyCommandDisplay($value);
+		FlyCommandReturn($value);
+		break;
+
+	case 'set':
+		// 0: "set", 1: key, 2: value
+		$return = FlyRegistrySet($cmd[1],$cmd[2]);
+		if ($return) {
+			FlyCommandDisplay('User registry key "'.$cmd[1].'" has been set to value "'.$cmd[2].'" for application "'.$_FLY['APP']['ID'].'" for user "'.$_FLY['USER']['NAME'].'" ('.$_FLY['USER']['ID'].').');
+			FlyCommandReturn('true');
+		} else {
+			FlyCommandDisplay('An error occurred while setting the registry key.');
+			FlyCommandReturn('false');
+		}
+		break;
+
+	case 'gset':
+		$return = FlyGlobalRegistrySet($cmd[1],$cmd[2]);
+		if ($return) {
+			FlyCommandDisplay('Global registry key "'.$cmd[1].'" has been set to value "'.$cmd[2].'" for application "'.$_FLY['APP']['ID'].'".');
+			FlyCommandReturn('true');
+		} else {
+			FlyCommandDisplay('An error occurred while setting the registry key.');
+			FlyCommandReturn('false');
+		}
+		break;
+
+	case 'remove':
+		// 0: "remove", 1: key, 2: application
+		$return = FlyRegistryRemove($cmd[1],$cmd[2]);
+		if (empty($cmd[2])) {
+			$app = $_FLY['APP']['ID'];
+		} else {
+			$app = $cmd[2];
+		}
+		if ($return) {
+			FlyCommandDisplay('User registry key "'.$cmd[1].'" has been removed from application "'.$app.'" for user "'.$_FLY['USER']['NAME'].'" ('.$_FLY['USER']['ID'].').');
+			FlyCommandReturn('true');
+		} else {
+			FlyCommandDisplay('An error occurred while removing the registry key.');
+			FlyCommandReturn('false');
+		}
+		break;
+
+
+	case 'gremove':
+		$return = FlyGlobalRegistryRemove($cmd[1],$cmd[2]);
+		if (empty($cmd[2])) {
+			$app = $_FLY['APP']['ID'];
+		} else {
+			$app = $cmd[2];
+		}
+		if ($return) {
+			FlyCommandDisplay('Global registry key "'.$cmd[1].'" has been removed from application "'.$app.'".');
+			FlyCommandReturn('true');
+		} else {
+			FlyCommandDisplay('An error occurred while removing the registry key.');
+			FlyCommandReturn('false');
+		}
+		break;
+
+	case 'listkeys':
+		// 0: "listkeys", 1: application
+		$keys = FlyRegistryListKeys($cmd[1]);
+		if (empty($cmd[1])) {
+			$app = $_FLY['APP']['ID'];
+		} else {
+			$app = $cmd[1];
+		}
+		if ($keys) {
+			FlyCommandReturn(json_encode($keys));
+			$display = '';
+			foreach ($keys as $k) {
+				$display .= $k.', ';
+			}
+			$display = str_lreplace(', ','',$display);
+			FlyCommandDisplay($display);
+		} else {
+			FlyCommandDisplay('An error occurred while retrieving the user registry keys for the application "'.$app.'" for user "'.$_FLY['USER']['NAME'].'" ('.$_FLY['USER']['ID'].').');
+			FlyCommandReturn('false');
+		}
+		break;
+
+	case 'glistkeys':
+		$keys = FlyGlobalRegistryListKeys($cmd[1]);
+		if (empty($cmd[1])) {
+			$app = $_FLY['APP']['ID'];
+		} else {
+			$app = $cmd[1];
+		}
+		if ($keys) {
+			FlyCommandReturn(json_encode($keys));
+			$display = '';
+			foreach ($keys as $k) {
+				$display .= $k.', ';
+			}
+			$display = str_lreplace(', ','',$display);
+			FlyCommandDisplay($display);
+		} else {
+			FlyCommandDisplay('An error occurred while retrieving the global registry keys for the application "'.$app.'" for user "'.$_FLY['USER']['NAME'].'" ('.$_FLY['USER']['ID'].').');
+			FlyCommandReturn('false');
+		}
+		break;
+
+	default:
+		FlyCommandDisplay('Invalid parameter! Expecting "get", "set", "remove", "getkeys", "listkeys", "gget", "gset", "gremove", "ggetkeys", or "glistkeys".');
+		FlyCommandReturn('false');
+		FlyCommandError('shell.dialog(\'Invalid parameter\',\'registry: Expecting "get", "set", "remove", "getkeys", "listkeys", "gget", "gset", "gremove", "ggetkeys", or "glistkeys".\',\'Invalid Parameter\',\''.$_FLY['RESOURCE']['URL']['ICONS'].'error.svg\');');
+}
 
 ?>
