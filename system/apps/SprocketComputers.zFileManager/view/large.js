a86316625468a2dfa703be632031b787c6ead2f0
Bryce <kitkatzecat@outlook.com>
2019-01-12 16:38:09 -0600

    Added other sizes of icon views

---
+++ system/apps/SprocketComputers.zFileManager/view/large.js
@@ -0,0 +1,53 @@
+function View() {
+	if (Folder && List) {
+		// Item object
+		var item = document.createElement('div');
+		item.className = 'FlyUiNoSelect FlyUiMenuItem FlyUiText';
+
+		item.style.display = 'inline-block';
+		item.style.boxSizing = 'border-box';
+		item.style.padding = '6px';
+		item.style.marginTop = '6px';
+		item.style.marginLeft = '6px';
+		item.style.cursor = 'default';
+		item.style.overflow = 'hidden';
+
+		item.style.height = 'auto';
+		item.style.width = '96px';
+
+		item.style.verticalAlign = 'top';
+		item.style.wordWrap = 'break-word';
+		item.style.textAlign = 'center';
+		item.style.fontSize = '14px';
+
+		for (var i = 0; i < List.length; i++) {
+			var itm = item.cloneNode(true);
+			var icn;
+			
+			itm.id = i;
+			
+			icn = Icon(List[i]);
+			icn.style.display = 'block';
+			icn.style.width = '64px';
+			icn.style.height = '64px';
+			icn.style.margin = '0 auto';
+			icn.style.marginBottom = '4px';
+			itm.appendChild(icn);
+
+			itm.innerHTML += List[i]['fname'];
+			itm.title = List[i]['name'];
+			itm.draggable = true;
+			
+			itm.onmousedown = function(e) {
+				Select(this,e,List[this.id]);
+			}
+			itm.ondblclick = function() {
+				Click(List[this.id]);
+			}
+			
+			document.body.appendChild(itm);
+		}
+	} else {
+		document.body.innerHTML += Output;
+	}
+}
