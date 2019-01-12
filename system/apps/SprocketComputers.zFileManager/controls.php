8505089bed25e7eabd45395ff4ac8bfb656afc59
Bryce <kitkatzecat@outlook.com>
2019-01-12 15:46:26 -0600

    Controls for other applications (in development)

---
+++ system/apps/SprocketComputers.zFileManager/controls.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+<?php
+include 'Fly.Standard.php';
+?>
+</head>
+<script>
+Fly.window.ready = function() {
+
+}
+var Parent;
+</script>
+<body>
+	
+</body>
+</html>
