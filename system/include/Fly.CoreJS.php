4a766f148107c743ffe19f2a22aeed00a1691332
Bryce <kitkatzecat@outlook.com>
2019-01-12 14:56:59 -0600

    Now attaches to standard Fly object + defines Fly if not defined

--- system/include/Fly.CoreJS.php
+++ system/include/Fly.CoreJS.php
@@ -7,6 +7,11 @@ FlyIncludeRegister('FLY.COREJS');
 echo '
 <script>
 var FlyCore = JSON.parse(atob(\''.base64_encode(json_encode($_FLY)).'\'));
+if (typeof Fly != \'undefined\') {
+	Fly.core = FlyCore;
+} else {
+	var Fly = {};
+}
 </script>
 ';
 ?>
