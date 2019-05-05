fa68bb9b598789116315459cef1bef920bb5f288
Bryce <kitkatzecat@outlook.com>
2019-05-05 01:35:33 -0500

    UI updates + basic functionality (incomplete: file only passed via "file", "path", "p")

--- system/apps/SprocketComputers.Utilities/openwith.php
+++ system/apps/SprocketComputers.Utilities/openwith.php
@@ -18,8 +18,12 @@ if ($process == false || $process['type'] !== 'file') {
 
 $typesXML = simpleXML_load_file($_FLY['RESOURCE']['PATH']['TYPES']);
 
+$list = [];
+$processes = [];
+
 $rec = '';
 $arec = [];
+$default = '';
 
 $lookup = FlyFileTypeLookup($process['extension']);
 $extension = $process['extension'];
@@ -31,7 +35,8 @@ if ($process['registered'] && $lookup['action'] !== '') {
 	}
 	if ($processapp != false && ($lookup['action'] != false && $lookup['action'] !== '')) {
 		$content = '"'.$process['description'].'" files are associated with "'.$processapp['name'].'".';
-		array_push($arec,$processapp['file']);
+		$default = $processapp;
+		$processes[$processapp['file']] = $processapp;
 		//$rec = '<div id="123456" class="FlyUiMenuItem FlyUiText FlyUiNoSelect"><img style="width:24px;height:24px;vertical-align:middle;margin-right:6px;" src="'.$processapp["icon"].'">'.htmlentities($processapp["name"]).'</div>';
 	} else {
 		if ((string)$typesXML->$extension->action == '' && isset($typesXML->$extension->app)) {
@@ -79,6 +84,7 @@ if ($process['registered'] && $lookup['action'] !== '') {
 			}
 			if ($processapp != false) {
 				array_push($arec,$processapp['file']);
+				$processes[$processapp['file']] = $processapp;
 				//$rec .= '<div id="'.$rand.'" class="FlyUiMenuItem FlyUiText FlyUiNoSelect"><img style="width:24px;height:24px;vertical-align:middle;margin-right:6px;" src="'.$processapp['icon'].'">'.htmlentities($processapp['name']).'</div>';
 			}
 		}
@@ -112,11 +118,14 @@ if (count($arec) > 0) {
 		$rand = rand();
 		$processrapp = FlyFileStringProcessor($r);
 		$rec .= '<div id="'.$rand.'" onclick="select(this)" class="FlyUiMenuItem FlyUiText FlyUiNoSelect"><img style="width:24px;height:24px;vertical-align:middle;margin-right:6px;" src="'.$processrapp['icon'].'">'.htmlentities($processrapp['name']).'</div>';
+		$list[$rand] = $processrapp['file'];
 	}
 }
 
 function getApps() {
-	$path = FLY_APPS_PATH;
+	global $_FLY;
+
+	$path = $_FLY['RESOURCE']['PATH']['APPS'];
 	$return = array();
 
 	$ignore = array( 'cgi-bin', '.', '..' ); 
@@ -141,6 +150,8 @@ function getApps() {
 
 function drawApps($array) {
 	global $_FLY;
+	global $list;
+	global $processes;
 	
 	$return = '';
 	foreach($array as $file => $app) {
@@ -148,7 +159,9 @@ function drawApps($array) {
 		if ($process != false) {
 			$rand = rand();
 			$return .= '<div id="'.$rand.'" onclick="select(this)" class="FlyUiMenuItem FlyUiText FlyUiNoSelect"><img style="width:24px;height:24px;vertical-align:middle;margin-right:6px;" src="'.$process["icon"].'">'.htmlentities($process["name"]).'</div>';
-			$xml = simpleXML_load_file(FLY_APPS_PATH.'/'.$file.'/ApplicationManifest.xml');
+			$list[$rand] = $process['file'];
+			$processes[$process['file']] = $process;
+			$xml = simpleXML_load_file($_FLY['RESOURCE']['PATH']['APPS'].'/'.$file.'/ApplicationManifest.xml');
 			if (isset($xml->masks)) {
 				$m = '';
 				foreach ($xml->masks->children() as $mask) {
@@ -157,6 +170,8 @@ function drawApps($array) {
 						if ($process != false) {
 							$rand = rand();
 							$m .= '<div id="'.$rand.'" onclick="select(this)" class="mask FlyUiMenuItem FlyUiText FlyUiNoSelect"><img style="width:24px;height:24px;vertical-align:middle;margin-right:6px;" src="'.$process["icon"].'">'.htmlentities($process["name"]).'</div>';
+							$list[$rand] = $process['file'];
+							$processes[$process['file']] = $process;
 						}
 					}
 				}
@@ -199,9 +214,10 @@ function drawApps($array) {
 	right: 117px;
 }
 .header {
-	padding-top: 8px;
-	padding-bottom: 2px;
-	margin-bottom: 4px;
+	margin-top: 8px;
+	padding-top: 4px;
+	padding-bottom: 8px;
+	margin-bottom: 0px;
 	font-size: 12px;
 	font-weight: bold;
 	position: sticky;
@@ -214,6 +230,11 @@ function drawApps($array) {
 }
 .mask {
 	display: none;
+}
+p.hint {
+	font-size: 0.8em;
+	margin-top: 0px;
+	margin-bottom: 0px;
 }
 </style>
 <script>
@@ -246,6 +267,15 @@ function select(obj) {
 		deselect(selected[i]);
 	}
 	obj.className = 'FlyUiMenuItemActive FlyUiText FlyUiNoSelect';
+
+	document.getElementById('always_label').innerHTML = 'Always open with "'+Processes[List[obj.id]]['fname']+'"';
+
+	window.submit = function() {
+		window.top.system.command('run:'+List[obj.id]+',file=<?php echo urlencode($_GET['file']); ?>,path=<?php echo urlencode($_GET['file']); ?>,p=<?php echo urlencode($_GET['file']); ?>');
+		Fly.window.close();
+	}
+
+	document.getElementById('button-ok').disabled = false;
 }
 function deselect(obj) {
 	obj.className = 'FlyUiMenuItem FlyUiText FlyUiNoSelect';
@@ -258,6 +288,10 @@ function toggleMasks() {
 		label.innerHTML = 'Show masks';
 	}
 }
+
+Fly.window.ready = function() {
+	Fly.window.message.show('Information: Implementation is incomplete. The specified file will be passed via parameters "file", "path", and "p".',10);
+}
 </script>
 
 <body onload="onload()" class="FlyUiNoSelect">
@@ -265,11 +299,21 @@ function toggleMasks() {
 <div class="main FlyUiText FlyUiNoSelect FlyUiContent">
 <div id="content">
 <div class="FlyUiMenuItem FlyUiText FlyUiNoSelect" style="margin-bottom:4px;"><img style="width:36px;height:36px;vertical-align:middle;margin-right:8px;" src="<?php echo $process['icon']; ?>"><?php echo $process['name']; ?></div>
-<?php echo $content; ?> Choose an application from the list below to open the file with:
+<p class="hint">Choose an application open this file with:</p>
 </div>
 </div>
 
 <div id="list" class="FlyUiText">
+
+<div class="header">Current Default</div>
+<?php 
+if ($default !== '') {
+	$rand = rand();
+	echo '<div id="'.$rand.'" onclick="select(this)" style="margin-bottom:4px;" class="FlyUiMenuItem FlyUiText FlyUiNoSelect"><img style="width:24px;height:24px;vertical-align:middle;margin-right:6px;" src="'.$default["icon"].'">'.htmlentities($default["name"]).'</div>';
+	$list[$rand] = $default['file'];
+}
+?>
+<p class="hint" style="margin-left:4px;"><?php echo $content; ?></p>
 
 <?php echo $rec; ?>
 <div class="header">All applications</div>
@@ -277,9 +321,16 @@ function toggleMasks() {
 <a id="masks-toggle">Show masks</a>
 </div>
 
-<div id="" style="position:absolute;bottom:62px;left:9px;" class="FlyUiTextHover"><input id="always" onchange="" type="checkbox"> <label for="always">Always use this application</label></div>
+<div id="" style="position:absolute;bottom:62px;left:9px;right:9px;padding:8px;margin:-8px;overflow:hidden;white-space:pre;text-overflow:ellipsis;" class="FlyUiTextHover"><input id="always" onchange="" type="checkbox"> <label for="always"><span id="always_label">Always open with this application</span></label></div>
 <button onclick="Fly.window.close();" id="button-close"><img src="<?php echo FLY_ICONS_URL; ?>mark-x.svg" style="width:16px;height:16px;vertical-align:middle;pointer-events:none;"></button>
-<button onclick="Fly.window.close();" id="button-ok" disabled><img src="<?php echo FLY_ICONS_URL; ?>mark-check.svg" style="width:16px;height:16px;vertical-align:middle;pointer-events:none;"></button>
+<button onclick="submit();" id="button-ok" disabled><img src="<?php echo FLY_ICONS_URL; ?>mark-check.svg" style="width:16px;height:16px;vertical-align:middle;pointer-events:none;"></button>
+
+<script>
+
+var List = JSON.parse(atob('<?php echo base64_encode(json_encode($list)); ?>'));
+var Processes = JSON.parse(atob('<?php echo base64_encode(json_encode($processes)); ?>'));
+
+</script>
 
 </body>
 </head>
