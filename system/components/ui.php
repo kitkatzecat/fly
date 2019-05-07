1b61a0caecd470a26f250dd78504d537bf681ac2
Bryce <kitkatzecat@outlook.com>
2019-05-07 16:09:54 -0500

    Increased speed of desktop icon fade

--- system/components/ui.php
+++ system/components/ui.php
@@ -367,7 +367,7 @@ ui.init = function() {
 		ui.desktop.style.right = '0px';
 		ui.desktop.style.bottom = '0px';
 		ui.desktop.style.zIndex = '-1';
-		ui.desktop.style.transition = 'opacity .3s linear';
+		ui.desktop.style.transition = 'opacity .2s linear';
 		ui.desktop.style.opacity = '0';
 		setTimeout(function(){
 			ui.desktop.innerHTML = '<iframe id="FlyUiDesktopFrame" style="width:100%;height:100%;" frameborder="0" allowtransparency="true" scrolling="no" src="<?php echo $_FLY['RESOURCE']['URL']['APPS']; ?>SprocketComputers.zFileManager/list.php?v=<?php echo FlyVarsReplace(FlyUserRegistryGet('ViewDesktop','SprocketComputers.zFileManager')); ?>&p=<?php echo FlyVarsReplace(FlyUserRegistryGet('DesktopPath','SprocketComputers.Options')); ?>"></iframe>';
