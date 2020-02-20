22e121daed62aa45ac571cd96499ee5054f08600
Bryce <kitkatzecat@outlook.com>
2020-02-20 00:14:12 -0600

    Will now catch parse errors and return error to cmd

--- system/components/cmd/php.php
+++ system/components/cmd/php.php
@@ -1,5 +1,9 @@
 <?php
-	$output = eval(implode(',',$cmd));
+	try {
+		$output = eval(implode(',',$cmd));
+	} catch(ParseError $e) {
+		$output = 'PHP Parse Error: '.$e->getMessage();
+	}
 	FlyCommandDisplay($output);
 	FlyCommandReturn($output);
 	FlyCommandError('shell.dialog(\'PHP\',\'Please use the command prompt to view output from this command.\',\'PHP\',\''.$_FLY['RESOURCE']['URL']['ICONS'].'error.svg\')');
