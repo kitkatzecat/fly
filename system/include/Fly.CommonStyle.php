2c2078d99f3ae94c91ad4ffae6354e0d7ce90472
Bryce <kitkatzecat@outlook.com>
2020-06-02 01:20:09 -0500

    Added Fly.CommonStyle include - CSS styles for common UI

---
+++ system/include/Fly.CommonStyle.php
@@ -0,0 +1,104 @@
+<style>
+body {
+	background-color: #fff;
+	padding-top: 8px;
+	padding-bottom: 24px;
+}
+h1,h2,h3,h4,h5,h6,p {
+	padding-left: 6%;
+	padding-right: 6%;
+}
+div.FlyCSTitle {
+	font-size: 1.2em;
+	font-weight: bold;
+	padding-top: 18px;
+	padding-bottom: 16px;
+	padding-left: 6%;
+	padding-right: 6%;
+}
+div.FlyCSTitle.FlyCSSectionTitle {
+    background-image: linear-gradient(to left, rgba(160,220,255,0.5) 0%,rgba(160,220,255,0) 100%);
+    margin-top: -8px;
+	padding-top: 24px;
+	position: relative;
+	overflow: hidden;
+}
+p.FlyCSDescription {
+	margin-top: -12px;
+}
+p.FlyCSHint {
+	font-size: 0.8em;
+	opacity: 0.8;
+	margin-top: -16px;
+	padding-left: 58px;
+}
+p.FlyCSParagraphTitle {
+	font-size: 0.8em;
+	opacity: 0.8;
+	margin-bottom: -16px;
+}
+img.FlyCSInlineIcon {
+	width: 18px;
+	height: 18px;
+	vertical-align: middle;
+	margin: 0px;
+	margin-right: 8px;
+	margin-top: -4px;
+}
+img.FlyCSTitleIcon {
+	width: 20px;
+	height: 20px;
+	vertical-align: middle;
+	margin: 0px;
+	margin-right: 8px;
+	margin-top: -4px;
+}
+img.FlyCSSectionIcon {
+	z-index: -1;
+	position: absolute;
+	right: 6%;
+	width: 79px;
+	height: 79px;
+	top: 0px;
+	filter: saturate(0) contrast(150%);
+}
+div.FlyCSBox {
+	background-color: rgba(0,0,0,0.05);
+	width: 100%;
+	box-sizing: border-box;
+	padding-top: 16px;
+	padding-bottom: 16px;
+	text-align: center;
+}
+div.FlyCSSticky {
+	background-color: #fff;
+	width: 100%;
+	box-sizing: border-box;
+	padding-top: 8px;
+	padding-bottom: 8px;
+	padding-left: 6%;
+	padding-right: 6%;
+	position: sticky;
+}
+div.FlyCSSticky.FlyCSStickyBottom {
+	border-top: 1px solid rgba(0,0,0,0.1);
+	bottom: 0px;
+	margin-top: 24px;
+}
+div.FlyCSSticky.FlyCSStickyTop {
+	border-bottom: 1px solid rgba(0,0,0,0.1);
+	top: 0px;
+	margin-bottom: 24px;
+}
+button,input[type=submit],input[type=reset],input[type=button] {
+	min-width: 100px;
+}
+img.FlyCSButtonIcon {
+	width: 16px;
+	height: 16px;
+	vertical-align: middle;
+}
+img.FlyCSButtonIcon.FlyCSButtonIconText {
+	margin-right: 6px;
+}
+</style>
