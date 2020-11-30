eef1bb668d2113c1a0a73d90403e5e526d483b96
Bryce <kitkatzecat@outlook.com>
2020-11-29 22:18:12 -0600

    Initial implementation of Jump Recent

--- system/apps/SprocketComputers.Utilities/jump/recent.php
+++ system/apps/SprocketComputers.Utilities/jump/recent.php
@@ -7,17 +7,22 @@ include 'Fly.Command.php';
 include 'Fly.Registry.php';
 include 'Fly.FileProcessor.php';
 
-$today = [
-	'SprocketComputers.zFileManager',
-	'SprocketComputers.Memo',
-	'SprocketComputers.MediaPlayer'
-];
-$yesterday = [
-	'SprocketComputers.PhotoViewer',
-	'SprocketComputers.zOptions',
-	'SprocketComputers.Preview',
-	'SprocketComputers.Utilities'
-];
+$apps = json_decode(FlyRegistryGet('ApplicationHistory','root.public'),true);
+
+$todayD = date("Y").str_pad(date("z"),3,'0',STR_PAD_LEFT);
+$yesterdayD = date("Y",strtotime('-1 day')).str_pad(date("z",strtotime('-1 day')),3,'0',STR_PAD_LEFT);
+
+if (isset($apps[$todayD])) {
+	$today = $apps[$todayD];
+} else {
+	$today = [];
+}
+
+if (isset($apps[$yesterdayD])) {
+	$yesterday = $apps[$yesterdayD];
+} else {
+	$yesterday = [];
+}
 
 $recent = ['Today'];
 foreach ($today as $app) {
@@ -34,22 +39,49 @@ foreach ($yesterday as $app) {
 	}
 }
 
-//$showID = FlyRegistryGet('JumpShowApplicationID','SprocketComputers.Utilities');
 ?>
 <link rel="stylesheet" href="items.css">
+<script>
+function contextMenu(file,item,e) {
+	Fly.actionmenu(e,[
+		[
+			'<b>Open</b>',
+			function() {
+				item.onclick();
+			},
+			{icon:'<?php echo $_FLY['RESOURCE']['URL']['ICONS']; ?>run.svg'}
+		],[
+			'Pin to Jump',
+			function() {
+				window.parent.run('SprocketComputers.Utilities.PinJump,file='+file);
+			},
+			{icon:'<?php echo $_FLY['RESOURCE']['URL']['ICONS']; ?>pin.svg'}
+		],[
+			'Remove from Recent',
+			function() {
+				Fly.command('history:remove,'+file,function() {
+					window.parent.nav('recent.php');
+				});
+			},
+			{icon:'<?php echo $_FLY['RESOURCE']['URL']['ICONS']; ?>mark-x.svg'}
+		]
+	]);
+	return false;
+}
+</script>
 </head>
 <body>
-<div style="margin-top:32px;text-align:center;">Not yet implemented</div>
 <?php
-/*
+
 foreach ($recent as $app) {
 	if (is_string($app)) {
 		echo '<div class="letter FlyUiNoSelect">'.$app.'</div>';
 	} else {
-		echo '<div class="FlyUiMenuItem FlyUiNoSelect"><img class="icon" src="'.$app['icon'].'">'.$app['fname'].'</div>';
+		if (!$app[''])
+		echo '<div oncontextmenu="return contextMenu(\''.$app['file'].'\',this,event);" onclick="window.parent.run(\''.$app['file'].'\');" class="FlyUiMenuItem FlyUiNoSelect"><img class="icon" src="'.$app['icon'].'">'.$app['fname'].'</div>';
 	}
 }
-*/
+
 ?>
 
 </body>
