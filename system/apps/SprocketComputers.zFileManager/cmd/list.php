9392ea206dd67e403a9d8cfc5bb51946a68fec24
Bryce <kitkatzecat@outlook.com>
2021-03-07 12:46:28 -0600

    Fixed sorting of files/folders

--- system/apps/SprocketComputers.zFileManager/cmd/list.php
+++ system/apps/SprocketComputers.zFileManager/cmd/list.php
@@ -13,6 +13,38 @@ if (file_exists($path)) {
 	$contents = array_diff($contents, ['..', '.']);
 	$contents = array_values($contents);
 
+	$folders = [];
+	$files = [];
+
+	foreach ($contents as $item) {
+		if (is_dir("$path/$item")) {
+			array_push($folders,$item);
+		} else {
+			array_push($files,$item);
+		}
+	}
+
+	if ($ARGUMENTS['sort'] == 'mime') {
+		natcasesort($folders);
+
+		$files_mime = [];
+		foreach ($files as $i => $file) {
+			$mime = mime_content_type("$path/$file");
+			$files_mime[$i] = $mime;
+		}
+		natcasesort($files_mime);
+		$files_new = [];
+		foreach ($files_mime as $file => $mime) {
+			array_push($files_new,$files[$file]);
+		}
+		$files = $files_new;
+	} else {
+		natcasesort($folders);
+		natcasesort($files);
+	}
+
+	$contents = array_merge($folders,$files);
+
 	$return = json_encode($contents);
 	goto r;
 } else {
