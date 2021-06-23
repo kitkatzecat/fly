61d103c1619fed0d6bb54bdc25f4ad2810e04a7d
Bryce <kitkatzecat@outlook.com>
2021-06-22 23:16:30 -0500

    Added older times + not showing headers if empty

--- system/apps/SprocketComputers.Utilities/jump/recent.php
+++ system/apps/SprocketComputers.Utilities/jump/recent.php
@@ -11,6 +11,10 @@ $apps = json_decode(FlyRegistryGet('ApplicationHistory','root.public'),true);
 
 $todayD = date("Y").str_pad(date("z"),3,'0',STR_PAD_LEFT);
 $yesterdayD = date("Y",strtotime('-1 day')).str_pad(date("z",strtotime('-1 day')),3,'0',STR_PAD_LEFT);
+$weekStartD = date("Y",strtotime('-2 days')).str_pad(date("z",strtotime('-2 days')),3,'0',STR_PAD_LEFT);
+$weekEndD = date("Y",strtotime('-7 days')).str_pad(date("z",strtotime('-7 days')),3,'0',STR_PAD_LEFT);
+$monthStartD = date("Y",strtotime('-8 days')).str_pad(date("z",strtotime('-8 days')),3,'0',STR_PAD_LEFT);
+$monthEndD = date("Y",strtotime('-30 days')).str_pad(date("z",strtotime('-30 days')),3,'0',STR_PAD_LEFT);
 
 if (isset($apps[$todayD])) {
 	$today = $apps[$todayD];
@@ -24,20 +28,62 @@ if (isset($apps[$yesterdayD])) {
 	$yesterday = [];
 }
 
-$recent = ['Today'];
-foreach ($today as $app) {
-	$process = FlyFileStringProcessor(FlyVarsReplace($app));
-	if ($process) {
-		array_push($recent,$process);
+$weekIteratorD = $weekStartD;
+$week = [];
+while ($weekIteratorD <= $weekStartD) {
+	if (isset($apps[$weekIteratorD])) {
+		$week = array_merge($week,$apps[$weekIteratorD]);
+	}
+	$weekIteratorD = $weekIteratorD+1;
+}
+
+$monthIteratorD = $monthStartD;
+$month = [];
+while ($monthIteratorD <= $monthStartD) {
+	if (isset($apps[$monthIteratorD])) {
+		$month = array_merge($month,$apps[$monthIteratorD]);
+	}
+	$monthIteratorD = $monthIteratorD+1;
+}
+
+$recent = [];
+if (count($today) > 0) {
+	array_push($recent,'Today');
+	foreach ($today as $app) {
+		$process = FlyFileStringProcessor(FlyVarsReplace($app));
+		if ($process) {
+			array_push($recent,$process);
+		}
 	}
 }
-array_push($recent,'Yesterday');
-foreach ($yesterday as $app) {
-	$process = FlyFileStringProcessor(FlyVarsReplace($app));
-	if ($process) {
-		array_push($recent,$process);
+if (count($yesterday) > 0) {
+	array_push($recent,'Yesterday');
+	foreach ($yesterday as $app) {
+		$process = FlyFileStringProcessor(FlyVarsReplace($app));
+		if ($process) {
+			array_push($recent,$process);
+		}
 	}
 }
+if (count($week) > 0) {
+	array_push($recent,'Last Week');
+	foreach ($week as $app) {
+		$process = FlyFileStringProcessor(FlyVarsReplace($app));
+		if ($process) {
+			array_push($recent,$process);
+		}
+	}
+}
+if (count($month) > 0) {
+	array_push($recent,'Last Month');
+	foreach ($month as $app) {
+		$process = FlyFileStringProcessor(FlyVarsReplace($app));
+		if ($process) {
+			array_push($recent,$process);
+		}
+	}
+}
+
 
 ?>
 <link rel="stylesheet" href="items.css">
